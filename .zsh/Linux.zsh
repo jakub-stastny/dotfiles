7097b07803c622bb2cd79bfe01180eaafcb96c83
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-01-14 19:41:34 -0300

    Notes.

--- .zsh/Linux.zsh
+++ .zsh/Linux.zsh
@@ -1,5 +1,8 @@
 dotfiles remote update
 
-if [ $(dotfiles rev-parse @) = $(dotfiles merge-base @ "$UPSTREAM") ]; then
+# @ refers to HEAD and @{u} to its upstream (origin/master).
+if [ $(dotfiles rev-parse @) = $(dotfiles rev-parse @{u}) ]; then
+  # TODO: Maybe handle push as well in case we're ahead.
+  # How about git stash?
   dotfiles pull -r && exec zsh || echo "Cannot update dotfiles."
 fi
