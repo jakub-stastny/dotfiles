8d02f765f17becb20709502152b33290a8c2d624
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-01-14 20:34:56 -0300

    Notes.

--- .zsh/Linux.zsh
+++ .zsh/Linux.zsh
@@ -1,6 +1,5 @@
 dotfiles remote update &> /dev/null
 
-# @ refers to HEAD and @{u} to its upstream (origin/master).
 # This setup was necessary: dotfiles branch --set-upstream-to=origin/master master
 # and dotfiles config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 #
@@ -10,9 +9,9 @@ dotfiles remote update &> /dev/null
 # doesn't work either. git branch -a shows only master, not remotes/origin/master
 # etc.
 #
+# @ refers to HEAD and @{u} to its upstream (origin/master).
 if [ $(dotfiles rev-parse @) != $(dotfiles rev-parse @{u}) ]; then
-  # TODO: Maybe handle push as well in case we're ahead.
-  # Then again we have the hook so we may pressume it has been pushed at all times.
+  # We are not handligh pushing, since we have the auto-push hook installed anyway.
   # How about git stash?
   dotfiles pull -r && exec zsh || echo "Cannot update dotfiles."
 fi
