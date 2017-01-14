71fb973adc0f79eb305e7cb1a2c781cd1aa905aa
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-01-14 20:31:40 -0300

    Might work.

--- .zsh/Linux.zsh
+++ .zsh/Linux.zsh
@@ -3,14 +3,14 @@ dotfiles remote update &> /dev/null
 # @ refers to HEAD and @{u} to its upstream (origin/master).
 # This setup was necessary: dotfiles branch --set-upstream-to=origin/master master
 # and dotfiles config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-if [ $(dotfiles rev-parse @) = $(dotfiles rev-parse @{u}) ]; then
+#
+# This solves the following issue:
 #
 # There's a bug, on the cloned repo the upstream ref doesn't work, origin/master
 # doesn't work either. git branch -a shows only master, not remotes/origin/master
 # etc.
 #
-# This is a work-around:
-# if [ $(dotfiles rev-parse @) != $(dotfiles rev-parse refs/heads/master) ]; then
+if [ $(dotfiles rev-parse @) = $(dotfiles rev-parse @{u}) ]; then
   # TODO: Maybe handle push as well in case we're ahead.
   # Then again we have the hook so we may pressume it has been pushed at all times.
   # How about git stash?
