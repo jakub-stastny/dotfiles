8f1e0e91b3be6028424bc1a08cb78b8140c7d25a
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-01-14 21:59:43 +0000

    No recursion.

--- .zsh/Linux.zsh
+++ .zsh/Linux.zsh
@@ -1,6 +1,5 @@
 dotfiles remote update
 
 if [ $(git rev-parse @) = $(git merge-base @ "$UPSTREAM") ]; then
-  dotfiles pull -r
-  exec zsh
+  dotfiles pull -r && exec zsh || echo "Cannot update dotfiles."
 fi
