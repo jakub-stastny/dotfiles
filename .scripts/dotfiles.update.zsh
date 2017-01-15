1e23cf2620bbf45663d9c8895de081f7d1c0fe03
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-01-14 23:28:28 -0300

    X

--- .scripts/dotfiles.update.zsh
+++ .scripts/dotfiles.update.zsh
@@ -2,7 +2,6 @@
 
 echo "~ Changed files:"
 git diff-tree -r --name-only --no-commit-id HEAD@{1}
-
 if git diff-tree -r --name-only --no-commit-id HEAD@{1} | grep vim; then
   echo "~ Updating Vim plugins."
   vim +PluginInstall +qall
