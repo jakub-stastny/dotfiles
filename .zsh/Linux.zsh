31f1f2e0791eba1b058f890cf0e713a90b408a8f
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-01-15 02:26:36 +0000

    Use fast forward.

--- .zsh/Linux.zsh
+++ .zsh/Linux.zsh
@@ -12,5 +12,9 @@ dotfiles remote update &> /dev/null
 if [ $(dotfiles rev-parse @) != $(dotfiles rev-parse @{u}) ]; then
   # We are not handling pushing, since we have the auto-push hook installed anyway.
   # Stashing the changes is ignored as well, pressumably I'd commit everything.
-  dotfiles pull -r && exec zsh || echo "Cannot update dotfiles."
+  #
+  # Note that we are using pull -ff, not -r, so we can use post-merge hook.
+  # There is no hook for git rebase. There is post-rewrite, but it works only
+  # when something is actually rewritten, not in fast-forward scenarios.
+  dotfiles pull --ff && exec zsh || echo "Cannot update dotfiles."
 fi
