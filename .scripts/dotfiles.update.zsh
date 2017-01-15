2957e5ece496f7bd609fbbdbe5532285c8afc0e0
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-01-14 23:32:48 -0300

    Post checkout hook.

--- .scripts/dotfiles.update.zsh
+++ .scripts/dotfiles.update.zsh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "~ Changed files:"
-git diff-tree -r --name-only --no-commit-id HEAD@{1}
+echo "~ Running post-checkout hook.\n\n"
+echo "~ Changed files: $(git diff-tree -r --name-only --no-commit-id HEAD@{1})"
 
 if git diff-tree -r --name-only --no-commit-id HEAD@{1} | grep vim; then
   echo "~ Updating Vim plugins."
