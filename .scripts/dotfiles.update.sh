9a327b3a44ee228f7fde5a5c49a5f6090f041c91
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-01-15 00:00:07 -0300

    Refactoring.

--- .scripts/dotfiles.update.sh
+++ .scripts/dotfiles.update.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-echo "~ Running post-checkout hook $*.\n"
-echo $1
-echo $2
-echo $3
+echo "~ Running post-checkout hook $1 -> $2.\n"
 echo "~ Changed files: $(git diff-tree -r --name-only --no-commit-id $1 $2)\n"
 
 if git diff-tree -r --name-only --no-commit-id HEAD@{1} | grep vim; then
