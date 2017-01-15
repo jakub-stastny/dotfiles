0f43f7bdefabc6478b936904bfd55fefc84be8bb
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-01-14 22:49:42 -0300

    SH compatibility, omit arguments that are not needed.

--- .scripts/dotfiles.install-remote.zsh
+++ .scripts/dotfiles.install-remote.zsh
@@ -10,8 +10,8 @@ fi
 REMOTE=$1
 
 # Functions.
-command() { echo "\$ $*" && $* }
-run_remote() { echo "$REMOTE \$ $*" && ssh $REMOTE $* }
+command() { echo "\$ $*" && $*; }
+run_remote() { echo "$REMOTE \$ $*" && ssh $REMOTE $*; }
 
 # Main.
 run_remote 'apt-get -y update && apt-get -y upgrade'
@@ -24,9 +24,9 @@ command scp ~/.ssh/id_rsa $REMOTE:~/.ssh/
 run_remote 'ssh-keyscan github.com >> ~/.ssh/known_hosts'
 run_remote 'git clone --bare git@github.com:botanicus/dotfiles.git'
 
-run_remote 'git --git-dir=$HOME/dotfiles.git --work-tree=$HOME config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"'
-run_remote 'git --git-dir=$HOME/dotfiles.git --work-tree=$HOME fetch'
-run_remote 'git --git-dir=$HOME/dotfiles.git --work-tree=$HOME branch --set-upstream-to=origin/master master'
+run_remote 'git --git-dir=$HOME/dotfiles.git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"'
+run_remote 'git --git-dir=$HOME/dotfiles.git fetch'
+run_remote 'git --git-dir=$HOME/dotfiles.git branch --set-upstream-to=origin/master master'
 
 # Note: At this moment the dotfiles alias is available locally, not remotely.
 run_remote 'git --git-dir=$HOME/dotfiles.git --work-tree=$HOME checkout'
