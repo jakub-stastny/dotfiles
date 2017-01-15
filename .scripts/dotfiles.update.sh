3ca42a21814329c0b6717f641d89117c3aee1070
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-01-15 03:31:30 +0000

    Fix reference.

--- .scripts/dotfiles.update.sh
+++ .scripts/dotfiles.update.sh
@@ -3,7 +3,7 @@
 echo "~ Running post-checkout hook $1 -> $2.\n"
 echo "~ Changed files: $(git diff-tree -r --name-only --no-commit-id $1 $2)\n"
 
-if git diff-tree -r --name-only --no-commit-id HEAD@{1} | grep vim; then
+if git diff-tree -r --name-only --no-commit-id $1 $2 | grep vim; then
   echo "~ Updating Vim plugins."
   vim +PluginInstall +qall
 fi
