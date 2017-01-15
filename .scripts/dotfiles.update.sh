71b068d2dfd53a46a9e2886cdc9f11d3f70a7f6a
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-01-14 23:52:23 -0300

    Get the vars.

--- .scripts/dotfiles.update.sh
+++ .scripts/dotfiles.update.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-echo "~ Running post-checkout hook.\n"
-echo "~ Changed files: $(git diff-tree -r --name-only --no-commit-id HEAD@{1})\n"
+echo "~ Running post-checkout hook $*.\n"
+echo $1
+echo $2
+echo $3
+echo "~ Changed files: $(git diff-tree -r --name-only --no-commit-id $1 $2)\n"
 
 if git diff-tree -r --name-only --no-commit-id HEAD@{1} | grep vim; then
   echo "~ Updating Vim plugins."
