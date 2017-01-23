0b0b107da9c05ca45a99b190a56cff2aeaf1c5c9
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-01-23 17:12:11 -0500

    Do not add certain commands to the history.

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -8,3 +8,22 @@ setopt inc_append_history
 #setopt share_history
 #
 setopt HIST_IGNORE_ALL_DUPS  # Remove old duplicates.
+
+HISTORY_BLACK_LIST=(
+  "launchctl unload ~/Library/LaunchAgents/botanicus.wifi.plist"
+  "launchctl unload ~/Library/LaunchAgents/botanicus.reenabler.plist"
+  "sudo vim /etc/hosts"
+)
+
+zshaddhistory() {
+  cmd=$(echo $1 | tr -d '\n')
+  index=${HISTORY_BLACK_LIST[(i)$cmd]}
+  if (( ${+HISTORY_BLACK_LIST[$index]} )); then
+    echo "~ Not adding to history."
+    return 1;
+  else
+    echo "~ Adding."
+  fi
+}
+
+# TODO: Dups are still being saved. https://github.com/mattjj/my-oh-my-zsh/blob/master/history.zsh
