00f0dec412b2eccbec7a94be8996cec006912e65
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-02-18 16:01:38 -0500

    Logs.

--- .bitbar/submenus/info.launchd.rb
+++ .bitbar/submenus/info.launchd.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/env ruby
 
-logs = Dir.glob('/tmp/botanicus.*.stderr').select do |path|
+logs = Dir.glob('/tmp/botanicus.*.stderr') + Dir.glob('/tmp/*.log')
+logs = logs.select do |path|
   (Time.now - (24 * 60 * 60)) < File.mtime(path)
 end
 
 unless logs.empty?
-  puts 'Recent launchd error logs | color=blue'
+  puts 'Recent error logs | color=blue'
   logs.each do |path|
-    puts "#{File.basename(path)} | bash='cat #{path}'"
+    puts "#{File.basename(path)} | bash='cat \"#{path}\"'"
   end
 end
