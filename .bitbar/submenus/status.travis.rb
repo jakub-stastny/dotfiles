d1488b57505b4bab8d64dab503bccf9662a2370d
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-02-18 18:13:53 -0500

    Fixes.

--- .bitbar/submenus/status.travis.rb
+++ .bitbar/submenus/status.travis.rb
@@ -7,11 +7,12 @@ require 'yaml'
 
 begin
   travis_config = YAML.load_file(File.expand_path('~/.travis/config.yml'))
-  client = Travis::Client.new(access_token: travis_config['access_token'])
+  endpoints = travis_config['endpoints']
+  client = Travis::Client.new(access_token: endpoints[endpoints.keys.first]['access_token'])
   active_repos = client.user.repositories.select { |repo| repo.active? }
   title 'Travis'
   active_repos.each do |repo|
-    title = "#{SYMBOLS[repo.color.to_sym]} #{repo.nickname}"
+    title = "#{SYMBOLS[repo.color.to_sym]} #{repo.login}"
     finished_at = format_time(repo.last_build_finished_at.localtime)
     stats = "#{repo.last_build_duration}s, #{finished_at}"
     href  = "https://travis-ci.org/#{client.user.name}/#{repo.name}"
