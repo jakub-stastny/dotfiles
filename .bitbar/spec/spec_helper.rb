7b37b6b9e1d3e9e2ae638b81bf8e11c12fb4ef81
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-02-20 15:49:37 -0500

    DBG

--- .bitbar/spec/spec_helper.rb
+++ .bitbar/spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'open3'
 class BitBarParser
   def initialize(plugin)
     @command = Dir.glob("./plugins/#{plugin}").first
+    p [:pwd, Dir.pwd] ###
   end
 
   def parse(extra_env = Hash.new)
