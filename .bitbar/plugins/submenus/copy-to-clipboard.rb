ff82974f90536d287f0ecaacf39d53058333fa75
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-02-26 16:49:13 -0500

    Improvements.

--- .bitbar/plugins/submenus/copy-to-clipboard.rb
+++ .bitbar/plugins/submenus/copy-to-clipboard.rb
@@ -4,11 +4,15 @@ load File.expand_path('../../../lib/bitbar.rb', __FILE__)
 
 require 'json'
 
+CMD_NAME = 'copy-to-clipboard'
+
 def command(identifier)
-  %Q{bash=copy-to-clipboard param1=#{identifier} terminal=false}
+  # Either of these work, but only with terminal=true.
+  %Q{bash='#{CMD_NAME} #{identifier}'}
+  # %Q{bash=#{CMD_NAME} param1=#{identifier} terminal=false}
 end
 
-data = JSON.parse(%x{copy-to-clipboard --list})
+data = JSON.parse(%x{#{CMD_NAME} --list})
 
 puts 'Copy to clipboard | color=green'
 puts '---'
