121971b9d34af2f1d9a8653b79700c56f7aaa2db
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2018-06-14 22:59:13 +0200

    NeoVim

---
+++ .local/share/nvim/snippets/helpers/complete.py
@@ -0,0 +1,15 @@
+def complete(t, opts):
+  print(t, opts)
+  if t:
+    opts = [ m[len(t):] for m in opts if m.startswith(t) ]
+  if len(opts) == 1:
+    return opts[0]
+  elif len(opts) > 1:
+    return "(" + "|".join(opts) + ")"
+  else:
+    return ""
+
+def fileNameAsConstant(snip):
+    chunks = snip.basename.split('_')
+    snip.rv = ''.join(chunk.title() for chunk in chunks)
+
