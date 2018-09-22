8cd1990e9e09ff5733e3d3022d6b4649e48ae05f
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2018-09-22 23:22:14 +0200

    Attach

--- .zsh/vagrant.zsh
+++ .zsh/vagrant.zsh
@@ -1,3 +1,5 @@
-export PATH=~/.gem/ruby/2.5.0/bin:$PATH
+export PATH=~/.gems/bin:$PATH
 cd ~/$(cat /etc/projectname)
 git install-hooks &> /dev/null # Override if there's something new.
+
+tmux a -t sys
