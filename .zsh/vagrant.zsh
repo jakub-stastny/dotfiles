01c27da61d7e9c262b64406fea64177e1a749cc6
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2018-09-22 23:46:57 +0200

    X

--- .zsh/vagrant.zsh
+++ .zsh/vagrant.zsh
@@ -2,4 +2,7 @@ export PATH=~/.gems/bin:$PATH
 cd ~/$(cat /etc/projectname)
 git install-hooks &> /dev/null # Override if there's something new.
 
+export GEM_HOME=~/.gems
+export GEM_PATH=~/.gems
+
 tmux a -t sys
