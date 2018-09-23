974482bc635f098b4c908945d0702833e92ba228
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2018-09-23 04:49:24 -0700

    Ref

--- .zsh/vagrant.zsh
+++ .zsh/vagrant.zsh
@@ -1,9 +1,13 @@
-export PATH=~/.gems/bin:$PATH
-cd ~/$(cat /etc/projectname)
-git install-hooks &> /dev/null # Override if there's something new.
+if test -d ~/.gems/bin; then
+  export PATH=~/.gems/bin:$PATH
+  export GEM_HOME=~/.gems
+  export GEM_PATH=~/.gems
+fi
 
-export GEM_HOME=~/.gems
-export GEM_PATH=~/.gems
+if test -f /etc/projectname; then
+  cd ~/$(cat /etc/projectname)
+  git install-hooks &> /dev/null # Override if there's something new.
+fi
 
 # This was in Linux.zsh and was working with mosh I think.
 # Start the sys session if there's none, so Mosh has somewhere to attach to.
