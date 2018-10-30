8b3060eea73feffebadec69f0f6cf9a436eb1abc
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2018-10-30 08:36:38 -0400

    X

--- .scripts/hours.rb
+++ .scripts/hours.rb
@@ -5,17 +5,19 @@ $LOAD_PATH.unshift('/root/hour-ruby/lib')
 require 'yaml'
 require 'hour'
 
+RATE = 50
+
 # This is interesting, YAML parses 0:15 as 900.
 timesheet = YAML.load(DATA.read)
-timesheet.each do |date, seconds|
-  puts "#{date} #{Hour.from(seconds: seconds)} – #{Hour.from(seconds: seconds).to_decimal}"
+hours = timesheet.map do |date, seconds|
+  hour_period = Hour.from(seconds: seconds)
+  puts "#{date} #{hour_period} – #{hour_period.to_decimal} = $#{(hour_period.to_decimal * RATE).round}"
+  hour_period
 end
 
+total = hours.reduce(:+)
+
+puts "\n#{total} * #{RATE} = $#{(total.to_decimal * RATE).round}"
+
 __END__
-Tue 16/10: 0:00:00
-Wed 17/10: 3:53:56
-Thu 18/10: 1:51:53
-Fri 19/10: 3:26:51
-Sat 20/10: 8:45:18
-Sun 21/10: 3:49:05
-Mon 22/10: 13:27:01
+Tue 30/10: 0:00:00
