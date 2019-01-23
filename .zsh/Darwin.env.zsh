4e8abe783847dda0c3e88d54e75724af280f0a3e
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2019-01-22 20:07:24 -0600

    X

--- .zsh/Darwin.env.zsh
+++ .zsh/Darwin.env.zsh
@@ -4,3 +4,8 @@ path-prepend ~/Dropbox/Data/Scripts
 
 # export rubylib=(~/Dropbox/Projects/Software/ruby/*/lib)
 # export projects=(~/Dropbox/Projects/{Software,}/*/*)
+
+# Homebrew doesn't link Ruby these days, it's keg-only.
+export PATH="/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/2.6.0/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/ruby/lib"
+export CPPFLAGS="-I/usr/local/opt/ruby/include"
