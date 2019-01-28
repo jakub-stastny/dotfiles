ff0f7002ab45908dab533cd4eb60f39038be196f
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2019-01-28 13:53:31 -0500

    X

--- .zsh/Linux.tmux.prompt.zsh
+++ .zsh/Linux.tmux.prompt.zsh
@@ -4,7 +4,7 @@ else
   user_colour=green
 fi
 
-PROMPT="%F{cyan}%1~%f $(ruby -v | awk '{ print $2 }' | sed 's/p.*//') %F{$user_colour}%#%F{reset} "
+PROMPT="%F{cyan}%1~%f %F{$user_colour}%#%F{reset} "
 RPROMPT='[%F{yellow}%?%f]'
 
 # source ~/.zsh/Darwin.prompt.zsh
