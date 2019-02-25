403a8803893fa3fa0e5d1e5edee3b2958c741951
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2019-02-25 17:11:52 -0500

    Creds

--- .zsh/setup.zsh
+++ .zsh/setup.zsh
@@ -5,6 +5,6 @@ else
   echo '~ Dropbox access token is not saved and $DROPBOX_ACCESS_TOKEN is not in the environment.'
 fi
 
-if !(( ${+PROWL_API_KEY} )); then
+if ! test -v PROWL_API_KEY; then
   echo '~ PROWL_API_KEY not defined.'
 fi
