040a6c089c4c6511c544816550e3e96f3210f4cd
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2019-02-25 16:23:40 -0500

    Expect PROWL_API_KEY

--- .zsh/setup.zsh
+++ .zsh/setup.zsh
@@ -1,8 +1,10 @@
-if ! test -f ~/.dropbox_uploader; then
-  if (( ${+DROPBOX_ACCESS_TOKEN} )); then
-    echo "OAUTH_ACCESS_TOKEN=$DROPBOX_ACCESS_TOKEN" | tee ~/.dropbox_uploader
-    echo "~ Saving Dropbox access token from the environment to the ~/.dropbox_uploader config."
-  else
-    echo '~ Dropbox access token is not saved and $DROPBOX_ACCESS_TOKEN is not in the environment.'
-  fi
+if ! test -f ~/.dropbox_uploader && (( ${+DROPBOX_ACCESS_TOKEN} )); then
+  echo "OAUTH_ACCESS_TOKEN=$DROPBOX_ACCESS_TOKEN" | tee ~/.dropbox_uploader
+  echo "~ Saving Dropbox access token from the environment to the ~/.dropbox_uploader config."
+else
+  echo '~ Dropbox access token is not saved and $DROPBOX_ACCESS_TOKEN is not in the environment.'
+fi
+
+if !(( ${+PROWL_API_KEY} )); then
+  echo '~ PROWL_API_KEY not defined.'
 fi
