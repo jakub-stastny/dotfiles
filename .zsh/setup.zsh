16f0bd37052d4dfc5101222c625e4a34933ebd41
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2019-02-26 16:12:07 +0000

    X

--- .zsh/setup.zsh
+++ .zsh/setup.zsh
@@ -1,7 +1,7 @@
 if ! test -f ~/.dropbox_uploader && (( ${+DROPBOX_ACCESS_TOKEN} )); then
   echo "OAUTH_ACCESS_TOKEN=$DROPBOX_ACCESS_TOKEN" | tee ~/.dropbox_uploader
   echo "~ Saving Dropbox access token from the environment to the ~/.dropbox_uploader config."
-else
+elif ! (( ${+DROPBOX_ACCESS_TOKEN} )); then
   echo '~ Dropbox access token is not saved and $DROPBOX_ACCESS_TOKEN is not in the environment.'
 fi
 
