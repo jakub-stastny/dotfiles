bb38682ae2f9b13718c265959966de756080b30c
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2019-04-17 17:28:52 +0000

    Prompt

--- .zsh/Linux.prompt.zsh
+++ .zsh/Linux.prompt.zsh
@@ -1,10 +1,2 @@
-if test $UID -eq 0; then
-  user_colour=red
-else
-  user_colour=green
-fi
-
-PROMPT="%F{cyan}☁  %F{$user_colour}%n%f@%F{blue}%m%f %F{yellow}%1~%f %# "
+PROMPT="%F{cyan}☁  %F{red}%n%f@%F{blue}%m%f %F{yellow}%1~%f %# "
 RPROMPT='[%F{yellow}%?%f]'
-
-# source ~/.zsh/Darwin.prompt.zsh
--- .zsh/host.zsh
+++ .zsh/host.zsh
@@ -4,6 +4,10 @@
 export cdpath=(. ~/projects)
 export EDITOR=vim
 
+# Prompt.
+PROMPT="%F{cyan}☁  %F{red}%n%f@%F{blue}%m%f %F{yellow}%1~%f %# "
+RPROMPT='[%F{yellow}%?%f]'
+
 # From aliases.zsh.
 function e () {
   test "$#" = 0 && $EDITOR . || $EDITOR $*
