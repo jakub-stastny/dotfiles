3e28ffa8917f2297b6518eddae9fc197e1338ae4
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2019-04-17 16:40:24 +0000

    Update fn

--- .zsh/host.zsh
+++ .zsh/host.zsh
@@ -1,1 +1,12 @@
 export cdpath=(. ~/projects)
+export EDITOR=vim
+
+# From aliases.zsh.
+function e () {
+  test "$#" = 0 && $EDITOR . || $EDITOR $*
+}
+
+# Update
+function update () {
+  wget https://raw.githubusercontent.com/botanicus/dotfiles/master/.zsh/host.zsh -O ~/.zshrc
+}
