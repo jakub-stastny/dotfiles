d98eaa0c1d1055e04569fd3198bc0ece3ddf5acc
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2020-10-29 06:12:26 -0400

    Auto tmux in DPM

--- .zsh/tmux-auto-connect.zsh
+++ .zsh/tmux-auto-connect.zsh
@@ -2,7 +2,8 @@
 # We do not want to have tmux-in-tmux, with 2 status lines and having to do C-b twice.
 #
 # https://stackoverflow.com/questions/27613209/how-to-automatically-start-tmux-on-ssh-session
-if [[ -z "$TMUX" ]] && [ "$SSH_CONNECTION" != "" ] && test -f /.dockerenv; then
+# if [[ -z "$TMUX" ]] && [ "$SSH_CONNECTION" != "" ] && test -f /.dockerenv; then
+if [[ -z "$TMUX" ]] && test -f /.dockerenv; then
   # Without -d (detach other clients), the screen wouldn't resize to fit
   # the current terminal resolution.
   tmux attach-session -d -t $USER@$HOST || tmux new-session -s $USER@$HOST
