4f05d3f2962cf9d79ed791b953c6aeacaa19657a
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2020-10-31 14:15:14 +0000

    Remove obsolete comment

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -10,7 +10,6 @@ path-clear()      { path=() }
 # Make $RUBYLIB and $rubylib behave like $PATH/$path.
 typeset -T -U -gx -a RUBYLIB rubylib ':'
 
-# This has to be here, because it's a dependency of dotfiles, it's not OS X specific.
 path-prepend ~/.scripts
 
 export EDITOR="nvim"
