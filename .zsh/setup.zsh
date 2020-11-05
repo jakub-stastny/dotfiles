4bcb14f834c9d2619aef02465c2796eafdad5224
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2020-11-05 00:20:32 +0000

    Shush

--- .zsh/setup.zsh
+++ .zsh/setup.zsh
@@ -26,7 +26,7 @@ zle -N edit-command-line
 bindkey '^xe' edit-command-line
 bindkey '^x^e' edit-command-line
 
-if (( ${+DOCKER_USERNAME} )) && (( ${+DOCKER_PASSWORD} )) && echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin; then
+if (( ${+DOCKER_USERNAME} )) && (( ${+DOCKER_PASSWORD} )) && echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin &> /dev/null; then
   echo "  $(tput setaf 2)✓$(tput sgr0) Logged into Docker Hub as $(tput setaf 7)$DOCKER_USERNAME$(tput sgr0)"
 else
   echo "  $(tput setaf 1)✘$(tput sgr0) Not logged into Docker Hub"
