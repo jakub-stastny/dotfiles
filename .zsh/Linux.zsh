66baecdf897aad85b46d2b3358458a45f11b3079
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2020-12-02 18:33:13 +0000

    Wait before clearing the screen

--- .zsh/Linux.zsh
+++ .zsh/Linux.zsh
@@ -20,7 +20,7 @@ if [ "$(dotfiles rev-parse @)" != "$(dotfiles rev-parse @{u})" ]; then
   # Note that post-rewrite works only when something is actually rewritten,
   # not in fast-forward rebasing scenarios.
   if dotfiles pull -r; then
-    clear; echo "$(tput setaf 2)Reloading ZSH ...$(tput sgr0)\n\n" && exec zsh
+    sleep 2; clear; echo "$(tput setaf 2)Reloading ZSH ...$(tput sgr0)\n\n" && exec zsh
   else
     echo "$(tput setaf 1)Cannot update dotfiles.$(tput sgr0)"
   fi
