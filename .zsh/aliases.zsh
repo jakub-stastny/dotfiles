be1b24100a674b766a8b8aebd1307db13153ecfb
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2021-04-15 04:42:47 -0400

    Add dpm-init

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -48,6 +48,11 @@ mksession() {
 yt='youtube-dl -f '\''bestaudio[ext=m4a]'\'
 
 # Projects
+dpm-init() {
+  test -d ~/projects || mkdir ~/projects
+  docker run -it --rm -v ~/projects:/projects jakubstastny/docker-project-manager init $1 ~/projects
+}
+
 p() {
   cd ~/projects/$(test "$#" -eq 0 && ls ~/projects | fzf || $1)
 }
