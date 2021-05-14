48b03b0c6108fff82a88ec326b64345e1d13e029
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2021-05-14 00:43:51 +0000

    Fix

--- .zsh/Linux.docker.env.zsh
+++ .zsh/Linux.docker.env.zsh
@@ -2,5 +2,7 @@
 #
 # NOTE: This has to go to project-specific configuration,
 # leaving here for now as a reference.
-# source /usr/local/share/chruby/chruby.sh
-# chruby $RUBY_VERSION
+#
+# Actually NO, it's required for ~/.scripts/capabilities.
+source /usr/local/share/chruby/chruby.sh
+chruby $RUBY_VERSION
