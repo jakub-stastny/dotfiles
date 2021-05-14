5ff9623b453a9bf6ebdddbcc34ce3801a83a84cd
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2021-05-14 12:03:52 +0000

    X

--- .zsh/tmux-auto-connect.zsh
+++ .zsh/tmux-auto-connect.zsh
@@ -4,6 +4,9 @@
 # https://stackoverflow.com/questions/27613209/how-to-automatically-start-tmux-on-ssh-session
 # if [[ -z "$TMUX" ]] && [ "$SSH_CONNECTION" != "" ] && test -f /.dockerenv; then
 if [[ -z "$TMUX" ]] && test -f /.dockerenv; then
+  # Use WORKDIR in your Dockerfile rather than trying to guess this.
+  # test -d $HOST && cd $HOST
+
   # Without -d (detach other clients), the screen wouldn't resize to fit
   # the current terminal resolution.
   tmux attach-session -d -t $HOST || tmux new-session -s $HOST
