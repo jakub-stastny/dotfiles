612368272323acc8dcb4735c92baec643f2ad3cd
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2021-05-15 13:44:12 +0000

    Fix mksession, note about dotfiles session

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -11,7 +11,6 @@ alias gst='git status'
 alias d='dotfiles'
 alias dp='dotfiles push'
 alias dst='dotfiles status'
-alias ddiff='dotfiles diff'
 
 # This defaults to NeoVim.
 # It's meant to be overwritten in per-project .profile.zsh files.
@@ -38,11 +37,14 @@ function mkpasswd() {
 }
 
 mksession() {
-  tmux new -s $(basename $PWD)
+  (cd $1 && tmux new -s $(basename $PWD))
 }
 
 yt='youtube-dl -f '\''bestaudio[ext=m4a]'\'
 
+# This should run in a new Tmux session (which is NOT nested).
+# We might even want to start the dotfiles session by default in tmux auto connect?
+# That way it's always pinned (as in position 0).
 edit-dotfiles() {
   test -S /tmp/emacs$(id -u)/dotfiles || emacs --daemon=dotfiles
 
