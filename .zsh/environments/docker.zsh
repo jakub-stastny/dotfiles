fd1cb4972281a3cd7ea0b15fa02aa38ec42cf3d8
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2021-06-03 19:17:17 +0000

    Fix

--- .zsh/environments/docker.zsh
+++ .zsh/environments/docker.zsh
@@ -1,7 +1,11 @@
 docker-login() {
   if (( ${+DOCKER_USERNAME} )) && (( ${+DOCKER_PASSWORD} )); then
-    test -f ~/.docker/config.json || docker-force-login
-    echo "  $(tput setaf 2)✓$(tput sgr0) Logged into Docker Hub as $(tput setaf 7)$DOCKER_USERNAME$(tput sgr0)."
+    if test -f ~/.docker/config.json; then
+      echo "  $(tput setaf 2)✓$(tput sgr0) Already logged into Docker Hub as $(tput setaf 7)$DOCKER_USERNAME$(tput sgr0)."
+    else
+      docker-force-login
+      echo "  $(tput setaf 2)✓$(tput sgr0) Logged into Docker Hub as $(tput setaf 7)$DOCKER_USERNAME$(tput sgr0)."
+    fi
   else
     echo "  $(tput setaf 1)✘$(tput sgr0) Not logged into Docker Hub, export $(tput setaf 7)\$DOCKER_USERNAME$(tput sgr0) and $(tput setaf 7)\$DOCKER_PASSWORD$(tput sgr0)"
   fi
