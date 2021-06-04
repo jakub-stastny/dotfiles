dbaee929595e89a9e80540784f0ebacad5064743
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2021-06-04 02:48:24 +0000

    Git env

---
+++ .zsh/environments/git.zsh
@@ -0,0 +1,5 @@
+# TODO: open file in a Vim with a template - and pass that into the -m message.
+release() {
+    # git tag ubuntu-21.04 -m "Ubuntu 21.04 with Emacs 27.1, NeoVim 0.4.4 and reworked dotfiles"
+    # git push --tags
+}
