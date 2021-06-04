5f65de129c5a69103d1b16a8a47c2b1bd314234c
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2021-06-04 03:17:35 +0000

    Print Docker container usage

--- .zsh/host.zsh
+++ .zsh/host.zsh
@@ -1,1 +1,9 @@
+print-docker-containers-usage() {
+  echo "$(tput setaf 5)$1: $(tput sgr0)CPU $(tput setaf 7)$2 $(tput sgr0)RAM $(tput setaf 7)$3 $(tput sgr0)($(tput setaf 7)$4$(tput sgr0)) PIDS $(tput setaf 7)$5$(tput sgr0)"
+}
+
+echo
+print-docker-containers-usage $(docker stats --no-stream | tail -n+2 | awk '{ print $2, $3, $4, $7, $14 }')
+echo
+
 test -d ~/projects && cd ~/projects
