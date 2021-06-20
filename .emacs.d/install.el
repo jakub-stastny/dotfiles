8c2ede4b6baf81356969092a02f4b8925ee767ed
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2021-06-20 20:41:39 +0000

    Emacs setup

--- .emacs.d/install.el
+++ .emacs.d/install.el
@@ -8,7 +8,7 @@
 
 (package-refresh-contents)
 
-(dolist (package '(org evil evil-commentary key-chord editorconfig))
+(dolist (package '(evil evil-commentary evil-collection key-chord editorconfig magit lispy lispyville))
   (unless (package-installed-p package)
     (message "~ Installing %s." package)
     (package-install package)))
