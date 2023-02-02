3afde18497c3203a1642f9bc57955968c477a25d
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2023-02-01 19:15:28 -0600

    Stuff from the old mac

--- .zsh/Darwin.env.zsh
+++ .zsh/Darwin.env.zsh
@@ -13,3 +13,16 @@ alias emacs="emacs -nw"
 # For compilers to find node@18 you may need to set:
 # export LDFLAGS="-L/opt/homebrew/opt/node@18/lib"
 # export (PPFLAGS="-I/opt/homebrew/opt/node@18/include"
+
+# From the old mac:
+##path-prepend ~/Dropbox/Projects/Software/*/*/bin
+#path-prepend /usr/local/bin
+#path-prepend ~/Dropbox/Data/Scripts
+
+## export rubylib=(~/Dropbox/Projects/Software/ruby/*/lib)
+## export projects=(~/Dropbox/Projects/{Software,}/*/*)
+
+## Homebrew doesn't link Ruby these days, it's keg-only.
+#export PATH="/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/2.6.0/bin:$PATH"
+#export LDFLAGS="-L/usr/local/opt/ruby/lib"
+#export CPPFLAGS="-I/usr/local/opt/ruby/include"
