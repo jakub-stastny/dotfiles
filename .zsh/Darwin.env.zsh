430a14b079db2f42d277d3015a8e395b6ec8330d
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2023-02-02 08:14:17 -0600

    More macOS settings

--- .zsh/Darwin.env.zsh
+++ .zsh/Darwin.env.zsh
@@ -1,14 +1,22 @@
-export cdpath=(. ~/projects)
+export cdpath=(. ~/Documents/Projects)
 
 path-prepend /opt/homebrew/bin
 path-prepend /opt/homebrew/Cellar/ruby/3.2.0/bin
 path-prepend /opt/homebrew/opt/node@18/bin
 path-prepend ~/Library/Python/3.9/bin
+path-add /Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/bin
 
 test $PWD = $HOME && cd ~/Documents
 
-alias zshrc="e ~/.zshrc; source ~/.zshrc"
+alias zshrc="e ~/.zsh/Darwin.env.zsh; source ~/.zsh/Darwin.env.zsh"
 alias emacs="emacs -nw"
+
+# Required by https://github.com/practicalli/clojure-deps-edn
+# https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+export XDG_CONFIG_HOME=~/.config
+
+export DROPBOX_DIR=~/Library/CloudStorage/Dropbox
+export GDRIVE_DIR=~/Library/CloudStorage/GoogleDrive-jakub.stastny.pt@gmail.com
 
 # For compilers to find node@18 you may need to set:
 # export LDFLAGS="-L/opt/homebrew/opt/node@18/lib"
